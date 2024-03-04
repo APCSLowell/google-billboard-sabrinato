86b3c527241c935288a9f5fd3c1a6e06e0c80408
sabrinato <143123906+sabrinato@users.noreply.github.com>
2024-03-04 13:58:49 -0800

    Update GoogleBillboard.java

--- src/main/java/GoogleBillboard.java
+++ src/main/java/GoogleBillboard.java
@@ -19,14 +19,9 @@ public class GoogleBillboard {
 	//Finish this function
 	public static boolean isPrime(){
 		boolean prime = true;
-		  if (num < 2){
-    		prime = false;
-  			}
-  
- 			 for (int i = 2; i <= (Math.sqrt(num)); i++){
-    			if (num % i == 0){
-    			  prime = false;
-    		}
+		if (num < 2){prime = false;}
+  		for (int i = 2; i <= (Math.sqrt(num)); i++){
+    			if (num % i == 0){prime = false;}
   		}
  		 return prime;
 	}
